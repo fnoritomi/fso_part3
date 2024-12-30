a8f7e008806b41973807cbc7b926b2ffc8a050f6
fnoritomi <34938550+fnoritomi@users.noreply.github.com>
2024-12-30 16:30:48 -0300

    Exercise 3.6 solution

--- index.js
+++ index.js
@@ -67,7 +67,19 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-      
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+          error: 'Name or number missing' 
+        })
+    }    
+
+    if (persons.find(p => p.name === body.name)) {
+        return response.status(400).json({ 
+          error: 'Name must be unique' 
+        })
+    }
+
     const person = {
         id: generateId(),        
         name: body.name,
