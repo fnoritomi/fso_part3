bf865cfb456f4f6129f6dda466a0dc58325cb896
fnoritomi <34938550+fnoritomi@users.noreply.github.com>
2024-12-30 16:02:25 -0300

    Exercise 3.2 solution

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const qtyRecords = persons.length
+    const date = new Date()
+    response.send(`<p>Phonebook has info for ${qtyRecords} ${qtyRecords > 1 ? 'people' : 'person'} </p><p> ${date} </p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
