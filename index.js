6bdee803c83f2e3ce1043be6bc681a890bda3e8e
fnoritomi <34938550+fnoritomi@users.noreply.github.com>
2025-01-03 16:32:02 -0300

    Exercises 3.7 and 3.8 solutions

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
+
+const log = morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens.data(req, res)
+    ].join(' ')
+  })
+
+app.use(log)
 
 let persons = [
     { 
